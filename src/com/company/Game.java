5abbfa99b3ae739e869250e527490480f23d2993
firey828 <47870754+firey828@users.noreply.github.com>
2020-05-28 14:31:27 -0400

    literally everything at all ever

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,16 +1,22 @@
 package com.company;
 
 public class Game {
+
+    // ======== INSTANCE VARIABLES ========
     private String name;
     private Problem[] problems;
     private Stars stars;
 
+    // ======== CONSTRUCTORS ========
     public Game(String user, Problem[] probs, Stars myStars) {
         name = user;
         problems = probs;
         stars = myStars;
     }
 
+    /*
+     * Does a game.
+     */
     public void play() {
         GameMethods gm = new GameMethods();
         int additionWrong = 0;
@@ -19,8 +25,12 @@ public class Game {
 
         for (int i = 0; i < problems.length; i++) {
             String numAsString;
-            for (numAsString = gm.ask(stars.toString() + "\n\n" + problems[i].toString()); numAsString.length() == 0 || gm.isInt(numAsString) || numAsString.charAt(0) == '0' && problems[i].getAnswer() != 0; numAsString = gm.ask(stars.toString() + "\n\n" + problems[i].toString())) {
-                gm.say("I'm sorry, " + name + ", but I couldn't understand what you meant!\nMake sure you're typing in numbers instead of words.\nFor example, instead of \"three\", type \"3\"!");
+            for (numAsString = gm.ask(stars.toString() + "\n" +
+                    "\n" + problems[i].toString()); numAsString.length() == 0 || gm.isInt(numAsString) || numAsString.charAt(0) == '0' && problems[i].getAnswer() != 0; numAsString = gm.ask(stars.toString() + "\n" +
+                    "\n" + problems[i].toString())) {
+                gm.say("I'm sorry, " + name + ", but I couldn't understand what you meant!\n" +
+                        "Make sure you're typing in numbers instead of words.\n" +
+                        "For example, instead of \"three\", type \"3\"!");
                 gm.say("Let's try that again!");
             }
 
@@ -39,23 +49,30 @@ public class Game {
             }
         }
 
-        double score = 100.0D * ((double) numCorrect / (double) problems.length);
+        double score = 100.0 * ((double) numCorrect / (double) problems.length);
         gm.say("You got " + score + "% ( " + numCorrect + " / " + problems.length + " )! ");
         gm.say("You got " + additionWrong + " addition problems wrong and " + subtractionWrong + " subtraction problems wrong! ");
-        if (score == 0.0D) {
-            gm.say("Oh no! You didn't get any of the problems right!\nAsk a parent or your teacher to help you with the things you don't understand!");
-        } else if (score <= 25.0D) {
-            gm.say("You got a lot of the problems wrong!\nAsk a parent or your teacher to help you with the things you don't understand!");
-        } else if (score <= 50.0D) {
-            gm.say("You got a few of the problems wrong!\nAsk a parent or your teacher to help you with the things you don't understand!");
-        } else if (score <= 75.0D) {
-            gm.say("You did good, but you can always do better!\nAsk a parent or your teacher to help you with the things you don't understand!");
-        } else if (score <= 90.0D) {
-            gm.say("You're almost there! If you play again, I'm sure you\ncan get an even higher score! I believe in you, " + name + "! ");
-        } else if (score <= 99.0D) {
-            gm.say("Congratulations, " + name + "! You're a math star!\nPlay again and try to get an even higher score!");
+        if (score == 0.0) {
+            gm.say("Oh no! You didn't get any of the problems right!\n" +
+                    "Ask a parent or your teacher to help you with the things you don't understand!");
+        } else if (score <= 25.0) {
+            gm.say("You got a lot of the problems wrong!\n" +
+                    "Ask a parent or your teacher to help you with the things you don't understand!");
+        } else if (score <= 50.0) {
+            gm.say("You got a few of the problems wrong!\n" +
+                    "Ask a parent or your teacher to help you with the things you don't understand!");
+        } else if (score <= 75.0) {
+            gm.say("You did good, but you can always do better!\n" +
+                    "Ask a parent or your teacher to help you with the things you don't understand!");
+        } else if (score <= 90.0) {
+            gm.say("You're almost there! If you play again, I'm sure you\n" +
+                    "can get an even higher score! I believe in you, " + name + "! ");
+        } else if (score <= 99.0) {
+            gm.say("Congratulations, " + name + "! You're a math star!\n" +
+                    "Play again and try to get an even higher score!");
         } else {
-            gm.say("Wow, " + name + "! You got everything right!\nYou aren't just a math star -- you're a math superstar!!");
+            gm.say("Wow, " + name + "! You got everything right!\n" +
+                    "You aren't just a math star -- you're a math superstar!!");
         }
 
         if (additionWrong > subtractionWrong) {
--- src/com/company/GameMethods.java
+++ src/com/company/GameMethods.java
@@ -6,83 +6,123 @@ import javax.swing.JOptionPane;
 class GameMethods {
 
     // ======== CONSTRUCTORS ========
-    GameMethods() {
+    public GameMethods() {
     }
 
-    boolean isInt(String s) {
+    /*
+     * Returns true if the String s represents a valid integer value, false otherwise
+     */
+    public boolean isInt(String s) {
         for(int i = 0; i < s.length(); ++i) {
             if (!Character.isDigit(s.charAt(i))) {
                 return true;
             }
         }
-
         return false;
     }
 
-    void sayRandomCongrats(String name) {
+    /*
+     * Displays a random selection from the following:
+     *
+     * "You're doing great, [NAME]!"
+     * "Nice job, [NAME]!"
+     * "You'll be a math star in no time, [NAME]!"
+     * "Good work, [NAME]!"
+     * "Wow! You're so smart, [NAME]!"
+     * "Well done, [NAME]!"
+     * "That was awesome, [NAME]!"
+     * "Keep going, [NAME]!"
+     * "You're almost there, [NAME]!"
+     * "Isn't this fun, [NAME]?"
+     * "You're a genius, [NAME]!"
+     */
+    public void sayRandomCongrats(String name) {
         double msgID = Math.random();
-        if (msgID == 0.0D) {
-            this.say("You're doing great, " + name + "!");
-        } else if (msgID <= 0.1D) {
-            this.say("Nice job, " + name + "!");
-        } else if (msgID <= 0.2D) {
-            this.say("You'll be a math star in no time, " + name + "!");
-        } else if (msgID <= 0.3D) {
-            this.say("Good work, " + name + "!");
-        } else if (msgID <= 0.4D) {
-            this.say("Wow! You're so smart, " + name + "!");
-        } else if (msgID <= 0.5D) {
-            this.say("Well done, " + name + "!");
-        } else if (msgID <= 0.6D) {
-            this.say("That was awesome, " + name + "!");
-        } else if (msgID <= 0.7D) {
-            this.say("Keep going, " + name + "!");
-        } else if (msgID <= 0.8D) {
-            this.say("You're almost there, " + name + "!");
-        } else if (msgID <= 0.9D) {
-            this.say("Isn't this fun, " + name + "?");
+        if (msgID == 0.0) {
+            say("You're doing great, " + name + "!");
+        } else if (msgID <= 0.1) {
+            say("Nice job, " + name + "!");
+        } else if (msgID <= 0.2) {
+            say("You'll be a math star in no time, " + name + "!");
+        } else if (msgID <= 0.3) {
+            say("Good work, " + name + "!");
+        } else if (msgID <= 0.4) {
+            say("Wow! You're so smart, " + name + "!");
+        } else if (msgID <= 0.5) {
+            say("Well done, " + name + "!");
+        } else if (msgID <= 0.6) {
+            say("That was awesome, " + name + "!");
+        } else if (msgID <= 0.7) {
+            say("Keep going, " + name + "!");
+        } else if (msgID <= 0.8) {
+            say("You're almost there, " + name + "!");
+        } else if (msgID <= 0.9) {
+            say("Isn't this fun, " + name + "?");
         } else {
-            this.say("You're a genius, " + name + "!");
+            say("You're a genius, " + name + "!");
         }
 
     }
 
-    void sayRandomCondolences(String name) {
+    /*
+     * Displays a random selection from the following:
+     *
+     * "Don't worry, [NAME]! You'll get it next time!"
+     * "So close!"
+     * "You're still a math star to me, [NAME]!"
+     * "Keep going! You can still make it!"
+     * "You tried your best, and that's what matters!"
+     * "We all make mistakes!"
+     * "That's okay! You can do it!"
+     * "You may have lost a star, but you're still a star to me, [NAME]!"
+     * "Come on, [NAME]! You can do it!"
+     * "Nobody's perfect!"
+     * "You're still doing great, [NAME]!"
+     */
+    public void sayRandomCondolences(String name) {
         double msgID = Math.random();
-        if (msgID == 0.0D) {
-            this.say("Don't worry, " + name + "! You'll get it next time!");
-        } else if (msgID <= 0.1D) {
-            this.say("So close!");
-        } else if (msgID <= 0.2D) {
-            this.say("You're still a math star to me, " + name + "!");
-        } else if (msgID <= 0.3D) {
-            this.say("Keep going! You can still make it!");
-        } else if (msgID <= 0.4D) {
-            this.say("You tried your best, and that's what matters!");
-        } else if (msgID <= 0.5D) {
-            this.say("We all make mistakes!");
-        } else if (msgID <= 0.6D) {
-            this.say("That's okay! You can do it!");
-        } else if (msgID <= 0.7D) {
-            this.say("You may have lost a star, but you're still a star to me, " + name + "!");
-        } else if (msgID <= 0.8D) {
-            this.say("Come on, " + name + "! You can do it!");
-        } else if (msgID <= 0.9D) {
-            this.say("Nobody's perfect!");
+        if (msgID == 0.0) {
+            say("Don't worry, " + name + "! You'll get it next time!");
+        } else if (msgID <= 0.1) {
+            say("So close!");
+        } else if (msgID <= 0.2) {
+            say("You're still a math star to me, " + name + "!");
+        } else if (msgID <= 0.3) {
+            say("Keep going! You can still make it!");
+        } else if (msgID <= 0.4) {
+            say("You tried your best, and that's what matters!");
+        } else if (msgID <= 0.5) {
+            say("We all make mistakes!");
+        } else if (msgID <= 0.6) {
+            say("That's okay! You can do it!");
+        } else if (msgID <= 0.7) {
+            say("You may have lost a star, but you're still a star to me, " + name + "!");
+        } else if (msgID <= 0.8) {
+            say("Come on, " + name + "! You can do it!");
+        } else if (msgID <= 0.9) {
+            say("Nobody's perfect!");
         } else {
-            this.say("You're still doing great, " + name + "!");
+            say("You're still doing great, " + name + "!");
         }
 
     }
 
-    void say(String msg) {
-        JOptionPane.showMessageDialog((Component)null, msg);
+    /*
+     * Produces a JOptionPane message dialogue pop-up with the String msg as its content.
+     */
+    public void say(String msg) {
+        JOptionPane.showMessageDialog(null, msg);
     }
 
-    String ask(String msg) {
+    /*
+     * Produces a JOptionPane input dialogue pop-up with the String msg as its content and returns
+     * user input as long as said input is not some capitalization of "exit". If it is, then the
+     * program will produce a pop-up that says "Goodbye." and the program will terminate.
+     */
+    public String ask(String msg) {
         String s = JOptionPane.showInputDialog(msg);
         if (s.equalsIgnoreCase("exit")) {
-            this.say("Goodbye.");
+            say("Goodbye.");
             System.exit(0);
         }
 
